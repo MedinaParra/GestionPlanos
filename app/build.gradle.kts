aaeeaa2357ca845d164971e26f9910eb0696904e
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-07-29 10:10:28 -0400

    Actualizar versión a flujo documental 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "cl.skmindustrial.gestionplanos"
     minSdk = 24
     targetSdk = 36
-    versionCode = 5
-    versionName = "5.0.0"
+    versionCode = 6
+    versionName = "6.0.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
